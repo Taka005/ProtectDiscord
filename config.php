932ed2892618e2d3a577b16a9f50e22a9c28f0a5
Taka005 <01taka005@gmail.com>
2022-11-26 19:01:01 +0900

    追加:基本システム構成

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$client_id = "";
+
+$secret_id = "";
+
+$scopes = "identify";
+
+$redirect_url = "";
+
+$token = "";
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+require __DIR__ . "/includes/discord.php";
+require __DIR__ . "/config.php";
+
+function is_animated($image){
+	$ext = substr($image, 0, 2);
+	if($ext == "a_"){
+		return ".gif";
+	}else{
+		return ".png";
+	}
+}
+?>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Protect Discord</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+	<main class="p-0 base">
+		<h1 class="text-center text-light">Protect Discord</h1>
+	</main>
+</body>
