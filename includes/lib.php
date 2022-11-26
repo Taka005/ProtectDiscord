3f19d5e23c4ada9cba6fff99c3d09360fb4aee26
Taka005 <01taka005@gmail.com>
2022-11-27 00:04:37 +0900

    変更:lib調整

---
+++ includes/lib.php
@@ -0,0 +1,17 @@
+<?php
+require "../config.php";
+
+function user($id,$token){
+    if(!is_numeric($id)) return false;
+    
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,"https://discord.com/api/v10/users/".$id); 
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_HTTPHEADER,array(
+        "Authorization: Bot ".$token,
+        "User-Agent:DiscordBot (https://reports.cf, 10)"
+    ));
+    $res =  curl_exec($ch);
+    curl_close($ch);
+    return json_decode($res,JSON_BIGINT_AS_STRING);
+}
