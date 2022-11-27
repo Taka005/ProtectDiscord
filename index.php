c0f04bf346b0264828909308c496cd29085e16ac
Taka005 <01taka005@gmail.com>
2022-11-27 11:05:27 +0900

    変更:テスト

--- index.php
+++ index.php
@@ -15,4 +15,5 @@ require "./config.php";
 	<main class="p-0 base">
 		<h1 class="text-center text-light">Protect Discord</h1>
 	</main>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.bundle.min.js" integrity="sha384-BOsAfwzjNJHrJ8cZidOg56tcQWfp6y72vEJ8xQ9w6Quywb24iOsW913URv1IS4GD" crossorigin="anonymous"></script>
 </body>
