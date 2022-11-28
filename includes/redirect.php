fe67f1043b811de05a6515ad1b0dd9ad93989d26
Taka005 <01taka005@gmail.com>
2022-11-28 17:58:34 +0900

    修正:転送リンク

---
+++ includes/redirect.php
@@ -0,0 +1,20 @@
+<?php
+require __DIR__."/discord.php";
+require __DIR__."/../config.php";
+
+if(isset($_POST["login"])){
+    header("Location:".url($client_id,$redirect_url,$scopes));
+}
+
+if(isset($_POST["logout"])){
+    header("Location: ./logout");
+}
+
+if(isset($_POST["terms"])){
+    header("Location: ../terms");
+}
+
+if(isset($_POST["privacy-policy"])){
+    header("Location: ../privacy-policy");
+}
+?>
