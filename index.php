79be88e6202f3db4e61e73989dbf6b65b2aac8cb
Taka005 <01taka005@gmail.com>
2022-11-29 12:15:49 +0900

    変更:不要なjs削除

--- index.php
+++ index.php
@@ -38,8 +38,6 @@ require __DIR__."/config.php";
         <form action="./search" method="get" class="mb-4 position-absolute top-50 start-50 translate-middle">
             <input name="id" type="number" class="form-control form-control-lg" placeholder="検索するユーザーID" autocomplete="off" required>
         </form>
-	</main>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+	</main>    
+    <script src="./assets/js/script.js"></script>
 </body>
