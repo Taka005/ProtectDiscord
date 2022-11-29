46a13e419489e9288beedfd4478aa0ce7a8abe2a
Taka005 <01taka005@gmail.com>
2022-11-29 11:09:51 +0900

    修正:バグ

--- search.php
+++ search.php
@@ -46,9 +46,7 @@ if(isset($_GET["id"])){
                 <div class="card-body">
                     <h5 class="card-title"><?= $user["username"]."#".$user["discriminator"] ?></h5>
                     <p class="card-text">製作中....</p>
-                    <form action="./report?=<?= $user["id"] ?>"  method="get">
-                        <a class="btn btn-danger">報告する</a>
-                    </form>
+                    <a href="./report?id=<?= $user["id"] ?> class="btn btn-danger">報告</a>
                     <a href="./" class="btn btn-outline-dark">戻る</a>
                 </div>
             </div>
