0d6431c941ddd150f57772874ad633a866210b35
Taka005 <01taka005@gmail.com>
2022-12-04 11:34:06 +0900

    変更:log

--- send.php
+++ send.php
@@ -12,7 +12,9 @@ if($_POST["id"]&&$_POST["class"]&&$_POST["content"]){
     if(isset($user)){
         $res = sql("SELECT * FROM tmp WHERE user = ".$user["id"]." LIMIT 1;")->fetch(PDO::FETCH_BOTH);
         if(!$res){
+            $id = id(18);
             sql("INSERT INTO tmp (time,reporter,user,class,content,id) VALUES (NOW(),".$_SESSION["user_id"].",".$user["id"].",'".$_POST["class"]."','".$_POST["content"]."','".id(18)."')");
+            sql("INSERT INTO log (time,reporter,user,reason,id,event) VALUES (NOW(),".$_SESSION["user_id"].",".$user["id"].",'".$_POST["class"]."','".id(18)."','報告')");
             $success = true;
         }else{
             $success = false;
