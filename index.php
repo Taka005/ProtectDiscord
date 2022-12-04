4e8100aba960c31d496be73242c3d7c95716a46a
Taka005 <01taka005@gmail.com>
2022-12-04 19:22:45 +0900

    修正:css

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ require_once __DIR__."/config.php";
         </header>
 	    <main>    
             <form action="./search" method="get" class="mb-4 position-absolute top-50 start-50 translate-middle">
-            <label for="exampleFormControlInput1" class="form-label text-center">ユーザーが登録されているかを確認できます</label>
+            <label for="exampleFormControlInput1" class="form-label text-center text-light">ユーザーが登録されているかを確認できます</label>
                 <input name="id" type="number" class="form-control form-control-lg" placeholder="検索するユーザーID" autocomplete="off" require_onced>
             </form>
 	    </main>   
