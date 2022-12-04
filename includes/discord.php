1128220a5ab0b472703ac53d181190ed3826cb42
Taka005 <01taka005@gmail.com>
2022-12-04 10:52:36 +0900

    修正:ミス

--- includes/discord.php
+++ includes/discord.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__."/lib.php";
+
 session_start();
 
 $GLOBALS["base_url"] = "https://discord.com";
--- includes/lib.php
+++ includes/lib.php
@@ -32,7 +32,7 @@ function id($n){
 }
 
 function sql($query){
-    $database = require_once __DIR__."/../config.php";
+    $database = require __DIR__."/../config.php";
     $pdo = new PDO("mysql:host=".$database["server"].";dbname=".$database["name"].";charset=utf8",$database["user"],$database["password"]);
     $res = $pdo->query($query);
     return $res;
